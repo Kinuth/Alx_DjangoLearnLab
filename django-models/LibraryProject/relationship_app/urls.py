878ab715a0e4dd5d8729b3a33ee3657fdf907abd
Kinuth <kinuthiaryan01@gmail.com>
2025-11-08 13:52:36 +0300

    updated relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,9 @@
 # relationship_app/urls.py
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
 from . import views
-from .views import LibraryDetailView
+from .views import LibraryDetailView, SignUpView
 from .views import list_books
 
 
@@ -9,9 +11,15 @@ app_name = 'relationship_app'
 
 urlpatterns = [
     # Function-Based View (FBV): Lists all books
-    path('books/', views.all_books_list, name='all_books'),
+    path('books/', views.list_books, name='all_books'),
 
     # Class-Based View (CBV): Shows details for a specific library
     # The <int:pk> captures the primary key (ID) of the Library
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+  
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/profile/',
+             TemplateView.as_view(template_name='accounts/profile.html'),
+             name='profile'),
+    path("signup/", SignUpView.as_view(), name="templates/registration/signup"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 # Create your views here.
 def list_books(request):
     """
@@ -29,4 +33,13 @@ class LibraryDetailView(DetailView):
         context['view_type'] = 'Class_Based DetailView'
         return context
     
+# User Signup View
+class SignUpView(CreateView):
+    """
+    View to handle user signup using Django's built-in UserCreationForm.
+    """
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')  # Redirect to login page after successful signup
+    template_name = 'registration/signup.html'
+
     
