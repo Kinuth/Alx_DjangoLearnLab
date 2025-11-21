0a0c88a0810b5eb197546f425a991af9b4ee315e
Kinuth <kinuthiaryan01@gmail.com>
2025-11-21 13:06:06 +0300

    Add initial API structure with models, migrations, and admin configuration

---
+++ api_project/api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
