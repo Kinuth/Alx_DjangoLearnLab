002c40a447174065d9254e451250fb33b85c69ba
Kinuth <kinuthiaryan01@gmail.com>
2025-11-22 15:35:29 +0300

    Add authentication and router configuration for Book API
    - Updated urls.py to use DefaultRouter and include token authentication endpoint.
    - Modified views.py to enforce authentication on BookViewSet.
    - Added 'rest_framework.authtoken' to INSTALLED_APPS in settings.py and configured authentication classes.

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 from .views import BookList
-from rest_framework import router
+from rest_framework import routers
 from .views import BookViewSet
 from django.urls import include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
-routers = router.DefaultRouter()
+routers = routers.DefaultRouter()
 routers.register(r'books_all', BookViewSet, basename='book_all')  # Registers the BookViewSet with the router 
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
  # Include the router URLs for BookViewSet (all CRUD operations)
-    path('', include(router.urls)),  # This includes all routes registered with the router
+    path('', include(routers.urls)),  # This includes all routes registered with the router
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     ]
