2418d878f9937ac8cceb39fce0d1234fa7a2bcbd
Kinuth <kinuthiaryan01@gmail.com>
2025-11-23 23:54:21 +0300

    Add secure proxy SSL header to settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -151,6 +151,8 @@ AUTH_USER_MODEL = 'bookshelf.CustomUser'
 #SECURE_HSTS_SECONDS = 31536000  # 1 year
 #SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 #SECURE_HSTS_PRELOAD = True
+#security settings end
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # --- Task 3 & 4: Secure Cookies ---
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
