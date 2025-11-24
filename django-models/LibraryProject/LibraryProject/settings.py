a976b5b0c48a6a3e5a44ea460efb2807389d80a0
Kinuth <kinuthiaryan01@gmail.com>
2025-11-24 11:32:57 +0300

    Fix relationship_app configuration in settings.py

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf',
-    'relationship_app.apps.RelationshipAppConfig',
-]
+    'relationship_app'
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
