e2a60cca175c5cd2139eabf93f06e0ee16629a19
Kinuth <kinuthiaryan01@gmail.com>
2025-12-05 19:19:39 +0300

    updated the add comment url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.PostDetailView.as_view(template_name='blog/post_detail.html'), name='detail_post'),
     path('post/<int:pk>/update/', views.update_post, name= 'update_post'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(template_name='blog/post_delete.html'), name='delete_post'),      
-    path('post/<int:pk>/comment/', views.CommentCreateView.as_view(), name='add_comment'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='add_comment'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 ]
