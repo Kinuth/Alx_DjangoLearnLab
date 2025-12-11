ca36d5b8cfab3322e9e5a322f2df92ed45e39c8b
Kinuth <kinuthiaryan01@gmail.com>
2025-12-11 16:51:34 +0300

    refactor: update user model and add follow/unfollow functionality
    - Changed User model to extend AbstractUser and removed username field.
    - Added following field to User model for tracking followed users.
    - Created FollowUserView and UnfollowUserView for managing follow relationships.
    - Updated serializers and views to accommodate changes in User model.
    - Added user feed endpoint to retrieve posts from followed users.
    - Removed obsolete UserAccount model and migration files.

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomAuthToken.as_view(), name='api_token_auth'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('follow/<int:pk>/', FollowUserView.as_view(), name='follow-user'),
-    path('unfollow/<int:pk>/', UnfollowUserView.as_view(), name='unfollow-user'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
