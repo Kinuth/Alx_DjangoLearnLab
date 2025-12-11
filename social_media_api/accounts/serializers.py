a48db595fcf71f4a29f42d905e1134bbad92b50f
Kinuth <kinuthiaryan01@gmail.com>
2025-12-11 12:37:00 +0300

    updated authentication to use Token Authentication

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     # Check requirement: explicitly using serializers.CharField
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
         model = User()
