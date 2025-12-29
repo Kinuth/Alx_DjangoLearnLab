d005f4a9558878c0ce769d99891bf3b6dc63c75e
Kinuth <kinuthiaryan01@gmail.com>
2025-12-29 10:31:53 +0300

    feat: restructure project for Vercel deployment with updated settings, requirements, and build scripts

--- social_media_api/build_files.sh
+++ social_media_api/build_files.sh
@@ -1,4 +1,10 @@
 # build_files.sh
-pip install -r requirements.txt
-python3.11 manage.py collectstatic
---noinput
+echo "Building the project..."
+python3.12 -m pip install -r requirements.txt
+
+echo "Make migrations..."
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
+
+echo "Collect static..."
+python3.12 manage.py collectstatic --noinput --clear
