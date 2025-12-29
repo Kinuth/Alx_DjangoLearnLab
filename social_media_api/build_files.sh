0e8d121f14772b640a3889ee794d7c5edadf9038
Kinuth <kinuthiaryan01@gmail.com>
2025-12-29 12:37:24 +0300

    Specify Python runtime version in Vercel configuration for social_media_api

--- social_media_api/build_files.sh
+++ social_media_api/build_files.sh
@@ -1,16 +1,10 @@
-#!/bin/bash
-
-# Create a fake pg_config so psycopg2-binary doesn't complain
-echo "Creating mock pg_config..."
-mkdir -p .bin
-echo "#!/bin/sh" > .bin/pg_config
-echo "echo '16.0'" >> .bin/pg_config
-chmod +x .bin/pg_config
-export PATH=$PATH:$(pwd)/.bin
-
+# build_files.sh
 echo "Installing dependencies..."
-python3.12 -m pip install --upgrade pip
-python3.12 -m pip install -r requirements.txt --prefer-binary
+# uv is pre-installed on Vercel's new builder
+uv pip install -r requirements.txt 
 
-echo "Collecting static..."
-python3.12 manage.py collectstatic --noinput --clear
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Running migrations..."
+python manage.py migrate --noinput
