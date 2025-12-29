4ca32a62a2e7023dbf3d8f4b1ea734bfb21392b8
Kinuth <kinuthiaryan01@gmail.com>
2025-12-29 10:45:04 +0300

    fix: use psycopg2-binary and removed django-heroku

--- social_media_api/build_files.sh
+++ social_media_api/build_files.sh
@@ -1,10 +1,8 @@
 # build_files.sh
-echo "Building the project..."
-python3.12 -m pip install -r requirements.txt
+echo "Installing dependencies..."
+# The --only-binary flag forces pip to use the pre-compiled version
+python3.12 -m pip install --upgrade pip
+python3.12 -m pip install -r requirements.txt --prefer-binary
 
-echo "Make migrations..."
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
-
-echo "Collect static..."
+echo "Collecting static files..."
 python3.12 manage.py collectstatic --noinput --clear
